83d70894dbc6802925aa387da9b819b874b2e621
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2023-01-29 11:47:13 +0100

    Lint

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,10 @@ if RUBY_ENGINE != "truffleruby"
   end
 end
 
+if RUBY_ENGINE == "truffleruby"
+  require "set" # rubocop:disable all
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "pairing_heap"
 
